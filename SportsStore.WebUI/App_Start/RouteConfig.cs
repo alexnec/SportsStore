42989b3489f4c16d133d23d3164ffd38692b8efd
Kosten <kosten_37@mail.ru>
2014-04-06 23:48:40 +0700

    Part of chapter 10

--- SportsStore.WebUI/App_Start/RouteConfig.cs
+++ SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -11,7 +11,7 @@ namespace SportsStore.WebUI
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            /*routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
                 name: null,
@@ -23,7 +23,38 @@ namespace SportsStore.WebUI
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Product", action = "List", id = UrlParameter.Optional }
+            );*/
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(null,
+                "",
+                new
+                {
+                    controller = "Product",
+                    action = "List",
+                    category = (string)null,
+                    page = 1
+                }
             );
+
+            routes.MapRoute(null,
+                "Page{page}",
+                new { controller = "Product", action = "List", category = (string)null },
+                new { page = @"\d+" }
+            );
+
+            routes.MapRoute(null,
+                "{category}",
+                new { controller = "Product", action = "List", page = 1 }
+            );
+
+            routes.MapRoute(null,
+                "{category}/Page{page}",
+                new { controller = "Product", action = "List" },
+                new { page = @"\d+" }
+            );
+
+            routes.MapRoute(null, "{controller}/{action}");
         }
     }
 }
