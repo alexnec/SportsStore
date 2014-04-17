0de06ceae999ecbc75514a139ddd03699ab6aca6
Kosten <kosten_37@mail.ru>
2014-04-17 22:33:12 +0700

    nublin

---
+++ SportsStore.WebUI/Controllers/CoursesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class CoursesController : Controller
+    {
+        //
+        // GET: /Courses/
+        public ActionResult Index()
+        {
+            ViewBag.Hello = "Хуй вам хохлы";
+            return View();
+        }
+	}
+}
